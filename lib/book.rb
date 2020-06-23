5466a846b760e1e181530b5d28d6f82c32819791
Jessica Pereira <jcpereira070@gmail.com>
2020-06-23 21:12:24 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title, :genre
 
   GENRES = ["Thriller", "Science Fiction", "Romance"]
@@ -11,6 +11,11 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre(genre)
+    @gengre  = genre
+    GENRES << genre
   end
 
 end
